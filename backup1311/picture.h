5ceffd811fa6e75a9fdf862f96c1869909ab6968
lsuutari19 <55877405+lsuutari19@users.noreply.github.com>
2020-11-13 14:46:21 +0200

    Add files via upload

---
+++ backup1311/picture.h
@@ -0,0 +1,22 @@
+/*
+ * picture.h
+ *
+ *  Created on: Nov 10, 2020
+ *      Author: lauri
+ */
+
+#ifndef PICTURE_H_
+#define PICTURE_H_
+
+#include <ti/mw/grlib/grlib.h>
+
+
+extern const tImage right;
+extern const tImage left;
+extern const tImage down;
+extern const tImage up;
+extern const tImage win;
+extern const tImage lose;
+
+#endif /* PICTURE_H_ */
+
